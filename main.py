47f1f223c89f1191689122bee1b60432d2ea8514
Patrick-Esg <patrickesguerra123@gmail.com>
2025-05-22 16:56:11 -0700

    4. Completely finished woohoo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, get_char_count , list_dictionary , sorter
+import sys
 
 #returns the contents of a text file with the path of file
 #input String path_of_file
@@ -7,19 +8,23 @@ def get_book_text(path_to_file):
         return f.read()
 
 def main():
-    frankenstein_text = get_book_text("/home/pat/workspace/github.com/Patrick-Esg/bookbot/books/frankenstein.txt")
-    char_count_dict = get_char_count(frankenstein_text)
+    print(sys.argv)
+    print("Usage: python3 main.py <path_to_book>")
+    #frankenstein_text = get_book_text("/home/pat/workspace/github.com/Patrick-Esg/bookbot/books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
+    char_count_dict = get_char_count(book_text)
     list_dict = list_dictionary(char_count_dict)
    
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")    
-    print("Found", get_word_count(frankenstein_text), "total words")
+    print("Found", get_word_count(book_text), "total words")
     print("--------- Character Count -------")
 
     for dic in sorter(char_count_dict):
         print(f"{dic["char"]}: {dic["num"]}")
 
     print("============= END ===============")
+    
 
 main()
